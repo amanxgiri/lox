c4577100c52d455a30f0b4f03479cbd078d4a6ed
Aman Kumar Giri <amangiri.dev@gmail.com>
2025-01-11 13:13:24 +0530

    Added control flow

--- jlox/Expr.java
+++ jlox/Expr.java
@@ -4,13 +4,32 @@ import java.util.List;
 
 abstract class Expr {
 	interface Visitor<R> {
+		R visitAssignExpr(Assign expr);
+
 		R visitBinaryExpr(Binary expr);
 
 		R visitGroupingExpr(Grouping expr);
 
 		R visitLiteralExpr(Literal expr);
 
+		R visitLogicalExpr(Logical expr);
+
 		R visitUnaryExpr(Unary expr);
+
+		R visitVariableExpr(Variable expr);
+	}
+
+	static class Assign extends Expr {
+	Assign(Token name,Expr value) {
+    this.name,Expr = name,Expr;
+    }
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitAssignExpr(this);
+		}
+
+	final Token name,Expr value;
 	}
 
 	static class Binary extends Expr {
@@ -56,6 +75,21 @@ abstract class Expr {
 		final Object value;
 	}
 
+	static class Logical extends Expr {
+	Logical(Expr left, Token operator,Expr right) {
+    this.left = left;
+    this.operator,Expr = operator,Expr;
+    }
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitLogicalExpr(this);
+		}
+
+		final Expr left;
+	final Token operator,Expr right;
+	}
+
 	static class Unary extends Expr {
 		Unary(Token operator, Expr right) {
 			this.operator = operator;
@@ -71,7 +105,18 @@ abstract class Expr {
 		final Expr right;
 	}
 
-	public Object expr;
+	static class Variable extends Expr {
+		Variable(Token name) {
+			this.name = name;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitVariableExpr(this);
+		}
+
+		final Token name;
+	}
 
 	abstract <R> R accept(Visitor<R> visitor);
 }
