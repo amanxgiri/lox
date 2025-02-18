686b262be4d71e2af672372d41d2428177fd5ab2
Aman Kumar Giri <amangiri.dev@gmail.com>
2025-02-19 03:30:23 +0530

    Added VM

--- clox/chunk.h
+++ clox/chunk.h
@@ -9,23 +9,38 @@
 
 // OP_CONSTANT -> 2 bytes long: one for opcode and other for operand
 // OP_RETRURN -> 1 byte long
+
+
+// OpCode --> represents a specific VM instruction and 
+// single byte long and tell what operation to perform
 typedef enum
 {
     OP_CONSTANT, // load constant value in VM's stack
     OP_RETURN,   // instruction will return from current function
 } OpCode;        // Opcode -> Operation code
 
+
+// Example :
+// Bytecode sequence for: constant 1.2; return;
+
+// Memory:  [OP_CONSTANT][1][OP_RETURN]
+// Offset:      0        1      2
+// Meaning:  Load const  #1   Return
+//           from slot 1
+
+
 typedef struct
 {
     int capacity;         //  size of the array
     int count;            //  no. of elements in the array
-    uint8_t *code;        // point to an array which is dynamic
+    uint8_t *code;        // point to an  dyanmic array of bytecode
+    int *lines;           // keeps track of line number of the code
     ValueArray constants; // stores the constants
 } Chunk;                  // holds the data along with instruction
 
-void initChunk(Chunk *chunk);                // initialising mem chunk
-void freeChunk(Chunk *chunk);                // free the memory allocated to prev chunk
-void writeChunk(Chunk *chunk, uint8_t byte); // appending a byte to end of array 'chunk'
-int addConstant(Chunk *chunk, Value value);  // appends constant to ValueArray or constant array
+void initChunk(Chunk *chunk);                          // initialising mem chunk
+void freeChunk(Chunk *chunk);                          // free the memory allocated to prev chunk
+void writeChunk(Chunk *chunk, uint8_t byte, int line); // appending a byte to end of array 'chunk' and what line number
+int addConstant(Chunk *chunk, Value value);            // appends constant to ValueArray or constant array
 
 #endif
--- clox/main.c
+++ clox/main.c
@@ -1,20 +1,25 @@
-
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[])
 {
+
+    initVM(); // starts the vm
+
     Chunk chunk;
     initChunk(&chunk);
 
     int constant = addConstant(&chunk, 1.2); // adds to constant pool
-    writeChunk(&chunk, OP_CONSTANT);         // Writes opcode
-    writeChunk(&chunk, constant);            // Write operand(index)
+    writeChunk(&chunk, OP_CONSTANT, 123);    // Writes opcode
+    writeChunk(&chunk, constant, 123);       // Write operand(index)
 
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_RETURN, 123);
 
-    disassemble(&chunk, "test_chunk");
+    disassembleChunk(&chunk, "test_chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
